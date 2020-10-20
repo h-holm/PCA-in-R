984489a62ddc20b0a9b7458ad5bb068298b19299
Henrik Holm <henholm@kth.se>
2020-10-20 16:21:07 +0200

    Add read counts to samples

--- PCA-of-genomes.R
+++ PCA-of-genomes.R
@@ -18,3 +18,17 @@ colnames(data.matrix) <- c(
 
 # Name samples gene1, gene2 etc.
 rownames(data.matrix) <- paste("gene", 1:100, sep="")
+
+# Give fake read counts to the genes.
+# Poisson ditribution instead of
+for (i in 1:100) {
+  # Since we're using made-up data, we can use the Poisson Distribution
+  # instead of the negative Binomial Distribution for simplicity.
+  wt.values <- rpois(5, lambda=sample(x=10:1000, size=1))
+  ko.values <- rpois(5, lambda=sample(x=10:1000, size=1))
+  
+  data.matrix[i,] <- c(wt.values, ko.values)
+}
+
+head(data.matrix)
+dim(data.matrix)
