15721bd7f33fbc57057713ee7a23656c218bd07c
Henrik Holm <henholm@kth.se>
2020-10-20 12:45:52 +0200

    Create made-up data

--- PCA-of-genomes.R
+++ PCA-of-genomes.R
@@ -6,3 +6,15 @@ for (i in listOfPackages){
   }
   require(i)
 }
+
+# Create made-up data.
+data.matrix <- matrix(nrow=100, ncol=10)
+
+# Let wt represent wild type (i.e. actual, everyday samples).
+# Let ko represent knock-out samples (i.e. samples with knocked out genes).
+colnames(data.matrix) <- c(
+  paste("wt", 1:5, sep=""),
+  paste("ko", 1:5, sep=""))
+
+# Name samples gene1, gene2 etc.
+rownames(data.matrix) <- paste("gene", 1:100, sep="")
