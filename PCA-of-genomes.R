d3a2e0ef912a4f5e0b02bb43a00d8ac510d13b41
Henrik Holm <henholm@kth.se>
2020-10-20 17:08:06 +0200

    Conform to style guide

--- PCA-of-genomes.R
+++ PCA-of-genomes.R
@@ -6,17 +6,17 @@ not_installed <- list_of_packages[!(list_of_packages %in% installed.packages()[,
 if (length(not_installed)) install.packages(not_installed)
 
 # Create made-up data.
-data.matrix <- matrix(nrow = 100, ncol = 10)
+data_matrix <- matrix(nrow = 100, ncol = 10)
 
 # Let wt represent wild type (i.e. actual, everyday samples).
 # Let ko represent knock-out samples (i.e. samples with knocked out genes).
-colnames(data.matrix) <- c(
+colnames(data_matrix) <- c(
   paste("wt", 1:5, sep = ""),
   paste("ko", 1:5, sep = "")
 )
 
 # Name samples gene1, gene2 etc.
-rownames(data.matrix) <- paste("gene", 1:100, sep = "")
+rownames(data_matrix) <- paste("gene", 1:100, sep = "")
 
 # Give fake read counts to the genes.
 # Poisson ditribution instead of
@@ -26,48 +26,47 @@ for (i in 1:100) {
   wt.values <- rpois(5, lambda = sample(x = 10:1000, size = 1))
   ko.values <- rpois(5, lambda = sample(x = 10:1000, size = 1))
 
-  data.matrix[i, ] <- c(wt.values, ko.values)
+  data_matrix[i, ] <- c(wt.values, ko.values)
 }
 
 # Samples are columns, genes are rows. Transpose to make data compatible with prcomp().
-# head(data.matrix)
-dim(data.matrix)
-data.matrix <- t(data.matrix)
-# head(data.matrix)
-dim(data.matrix)
+# head(data_matrix)
+dim(data_matrix)
+data_matrix <- t(data_matrix)
+dim(data_matrix)
 
 # Now we can test run PCA on our data.
-pca <- prcomp(data.matrix, scale = TRUE)
+pca <- prcomp(data_matrix, scale = TRUE)
 
 # Test plot using the two first principal components (out of 10 total).
 # The 1st account for the most variation, 2nd PC for second most etc.
 plot(pca$x[, 1], pca$x[, 2])
 
 # From the plot we could easily identify two clusters, each on opposite sides of the 1st PC's axis (x-axis).
-# print the variation of PC 1:
-pca.var <- (pca$sdev)^2
+# Get variation for each PC.
+pca_var <- (pca$sdev)^2
 
-# Calculate percentage of variation for current PC.
-pca.var.per <- round(pca.var / sum(pca.var) * 100, 1)
+# Calculate percentage of total variation for each PC.
+pca_var_prc <- round(pca_var / sum(pca_var) * 100, 1)
 
-# From the plot, we saw that PC1 accounts for almost all variation in the data:
-barplot(pca.var.per, main = "Scree Plot", xlab = "Principal Component", ylab = "Percent Variation")
+# From the following plot, we see that PC1 accounts for almost all variation in the data:
+barplot(pca_var_prc, main = "Scree Plot", xlab = "Principal Component", ylab = "Percent Variation")
 
 # If we want to investigate X/Y-coordinates for PCs 1 and 2, put them in dataframe.
-pca.data <- data.frame(
+pca_df <- data.frame(
   Sample = rownames(pca$x),
   X = pca$x[, 1],
   Y = pca$x[, 2]
 )
-pca.data
+pca_df
 
 # Plot PCs 1 and 2 using ggplot this time. Note variations on axes.
-ggplot(data = pca.data, aes(x = X, y = Y, label = Sample)) +
-  geom_text() +
-  xlab(paste("PC1 - ", pca.var.per[1], "%", sep = "")) +
-  ylab(paste("PC2 - ", pca.var.per[2], "%", sep = "")) +
-  theme_bw() + # Make background white.
-  ggtitle("My PCA")
+ggplot2::ggplot(data = pca_df, ggplot2::aes(x = X, y = Y, label = Sample)) +
+  ggplot2::geom_text() +
+  ggplot2::xlab(paste("PC1 - ", pca_var_prc[1], "%", sep = "")) +
+  ggplot2::ylab(paste("PC2 - ", pca_var_prc[2], "%", sep = "")) +
+  ggplot2::theme_bw() + # Make background white.
+  ggplot2::ggtitle("My PCA")
 
 # Check loading scores to determine which genes have the largest effect on where samples are plotted.
 # Focus on PC1 since it has the by far highest variation.
